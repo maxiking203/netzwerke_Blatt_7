789659f13e81dec61a72d385ac382bc1fcc81abd
maxiking203 <Maximilian.Sachmann@gmx.de>
2018-01-14 14:42:32 +0100

    Transistions for Receiver

--- src/Receiver/FileReceiver.java
+++ src/Receiver/FileReceiver.java
@@ -7,8 +7,55 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
 import Package.Package;
+import Sender.SenderMsg;
+import Sender.SenderState;
+
 
 public class FileReceiver {
+	
+	private ReceiverState currentState;
+	private Transition[][] trans = new Transition[ReceiverState.values().length][ReceiverMsg.values().length];
+	
+	{
+		trans[ReceiverState.WAIT.ordinal()][ReceiverMsg.recieved.ordinal()] = p -> {
+			System.out.println("Sending initial package.");
+			p = new Package();
+			return ReceiverState.CHECK_DATA;
+		};
+		
+		trans[ReceiverState.CHECK_DATA.ordinal()][ReceiverMsg.data_right.ordinal()] = p -> {
+			System.out.println("Sending initial package.");
+			p = new Package();
+			return ReceiverState.SEND_ACK_true;
+		};
+		
+		trans[ReceiverState.CHECK_DATA.ordinal()][ReceiverMsg.data_wrong.ordinal()] = p -> {
+			System.out.println("Sending initial package.");
+			p = new Package();
+			return ReceiverState.SEND_ACK_FALSE;
+		};
+		
+		trans[ReceiverState.CHECK_DATA.ordinal()][ReceiverMsg.last_right.ordinal()] = p -> {
+			System.out.println("Sending initial package.");
+			p = new Package();
+			return ReceiverState.GOT_LAST;
+		};
+		
+		trans[ReceiverState.GOT_LAST.ordinal()][ReceiverMsg.done.ordinal()] = p -> {
+			System.out.println("Sending initial package.");
+			p = new Package();
+			return ReceiverState.END;
+		};
+		
+	}
+	
+	
+	@FunctionalInterface
+	private interface Transition {
+		
+		ReceiverState execute(Package p);
+		
+	}
 	
 	public static void main(String[] args) throws SocketException {
 		
@@ -32,4 +79,5 @@ public class FileReceiver {
 			}
 		}		
 	}
+		
 }
