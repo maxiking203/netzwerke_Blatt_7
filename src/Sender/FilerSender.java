6df63add3cb3f62e71b8306d39318081b360e807
simbra92 <braeuersimon@gmx.de>
2018-01-15 15:39:04 +0100

    Sender 3 times timeout

--- src/Sender/FilerSender.java
+++ src/Sender/FilerSender.java
@@ -31,6 +31,7 @@ public class FilerSender {
 	private Package backupPacket;
 	private static boolean fin = false;
 	private Transition[][] trans = new Transition[SenderState.values().length][SenderMsg.values().length];
+	private static int failTime = 0;
 
 	
 	public FilerSender(String filename, InetAddress ip) {
@@ -97,56 +98,63 @@ public class FilerSender {
 	
 	private void waitForIncomingPacket() {
 		boolean gotpackage = false;
-		
-			
 			byte[] buffer = new byte[1400];
 			DatagramPacket incomingPacket = new DatagramPacket(buffer, buffer.length);
-			
-			while(!gotpackage) {
-				try {
-					sock.receive(incomingPacket);
-					Package pak = new Package(incomingPacket);
-					fin = pak.getFin();
-					Package test = new Package(pak.getFilename(), pak.getSeqNum(), pak.getAck(), pak.getFin(), pak.getContent());
-					long check = test.getCheckSum();
-					if (pak.getSeqNum() == seq) {
-						System.out.println("Seq in Ordnung");
-						if (pak.getAck()) {
-							System.out.println("Ack in Ordnung");
-							ack = true;
-							if (check == pak.getCheckSum()) {
-								System.out.println("Checksum passt");
-								System.out.println("Package erhalten");
-								System.out.println(pak.getAck() + "," + pak.getFilename() + "," + pak.getFin() + "," + pak.getSeqNum());
-								gotpackage = true;
-								if (!fin) {
-									prepare();
+			if (failTime < 3) {
+				while(!gotpackage) {
+					try {
+						sock.receive(incomingPacket);
+						failTime = 0;
+						Package pak = new Package(incomingPacket);
+						fin = pak.getFin();
+						Package test = new Package(pak.getFilename(), pak.getSeqNum(), pak.getAck(), pak.getFin(), pak.getContent());
+						long check = test.getCheckSum();
+						if (pak.getSeqNum() == seq) {
+							System.out.println("Seq in Ordnung");
+							if (pak.getAck()) {
+								System.out.println("Ack in Ordnung");
+								ack = true;
+								if (check == pak.getCheckSum()) {
+									System.out.println("Checksum passt");
+									System.out.println("Package erhalten");
+									System.out.println(pak.getAck() + "," + pak.getFilename() + "," + pak.getFin() + "," + pak.getSeqNum());
+									gotpackage = true;
+									if (!fin) {
+										prepare();
+									}
+								}
+								else {
+									System.out.println("Checksum falsch");
+									sendBackupPack();
 								}
 							}
 							else {
-								System.out.println("Checksum falsch");
+								System.out.println("Ack falsch");
 								sendBackupPack();
 							}
 						}
 						else {
-							System.out.println("Ack falsch");
+							System.out.println("Seq falsch");
 							sendBackupPack();
 						}
 					}
-					else {
-						System.out.println("Seq falsch");
+					catch (SocketTimeoutException s) {
+						System.out.println("Timeout");
+						failTime++;
+						if (failTime >= 3) {
+							break;
+						}
+						System.out.println(failTime);
 						sendBackupPack();
+						System.out.println("Resend Backup-Packet");
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
 				}
-				catch (SocketTimeoutException s) {
-					System.out.println("Timeout");
-					sendBackupPack();
-					System.out.println("Resend Backup-Packet");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 			}
-
+			else {
+				fin = true;
+			}
 	}
 	
 	private void sendBackupPack() {
@@ -231,7 +239,7 @@ public class FilerSender {
 			new FilerSender(args[0], InetAddress.getByName(args[1]));
 		} catch (ArrayIndexOutOfBoundsException e) {
 			try {
-				FilerSender fs = new FilerSender("default.txt", InetAddress.getByName("127.0.0.1"));
+				FilerSender fs = new FilerSender("default.txt", InetAddress.getByName("10.179.5.174"));
 				while(!fin) {
 					fs.prepare();
 					fs.waitForIncomingPacket();
